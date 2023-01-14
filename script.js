0d6126a2f553dffd7051cc6a7cf403582be00030
Ivon Ferreira <ivonferreira@users.noreply.github.com>
2023-01-14 17:53:37 -0300

    Tentando corrigir erro

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function clickMenu(){
     let menu = document.querySelector('nav#menu')
-    if (menu.style.display == 'none'){
-        menu.style.display = 'block';
-    }else{
+    if (menu.style.display == 'block'){
         menu.style.display = 'none';
+    }else{
+        menu.style.display = 'block';
     }
 }
