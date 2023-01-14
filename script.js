cac595b028c8cb1c2c8dd90c37e45edd19c3cba1
Ivon Ferreira <ivonferreira@users.noreply.github.com>
2023-01-14 18:19:56 -0300

    resolvido erro do menu

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+let menu = document.querySelector('nav#menu')
 function clickMenu(){
-    let menu = document.querySelector('nav#menu')
     if (menu.style.display == 'block'){
         menu.style.display = 'none';
     }else{
         menu.style.display = 'block';
+    }
+}
+function mudouTamanho(){
+    if (window.innerWidth >=700) {
+        menu.style.display = 'block';
+    }else{
+        menu.style.display = 'none';
     }
 }
